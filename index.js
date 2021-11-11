3712afc323f1eaa2b34cd34979e147b320491319
Dexie <35111165+Lamby777@users.noreply.github.com>
2021-11-11 17:13:26 +0000

    JSON responds to API

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/api/:mode", (req, res) => {
+	switch(req.params.mode) {
+		case "seeds":
+			res.json({
+				timestamp: new Date().getTime(),
+				miners: [],
+			});
+			break;
+		default:
+			res.send("404");
+	}
+});
+
 app.listen(3000, () => {
 	//
 });

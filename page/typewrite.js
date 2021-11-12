d227c4ac0de020520809fdc8496d5be178d36465
Dexie <35111165+Lamby777@users.noreply.github.com>
2021-11-12 06:46:13 +0000

    RNG rerolls if same number

--- page/typewrite.js
+++ page/typewrite.js
@@ -1,5 +1,6 @@
 // Used in index page to typewrite text
 const TYPE_SPEED = 100; // measured in ms per key
+const TEXT_KEEP = 5000; // time after finished to keep text
 const span = document.body.getElementById("homepage-edited-text");
 const ideaSlogans = [ // stereotypical corporate shit, lol
 	"A cryptocurrency for everyone",
@@ -7,13 +8,27 @@ const ideaSlogans = [ // stereotypical corporate shit, lol
 	"An NFT-first blockchain implementation",
 ];
 
-let lastIdeaNum;
+let lastIdeaNum = -1;
 
-function typewrite(text) {
+function typewrite(text, callback) {
+	let ideaNum;
+	do { // Assign new random numbers until not same as last
+		ideaNum = generateNewIdeaNum();
+	} while (ideaNum === lastIdeaNum);
+	lastIdeaNum = ideaNum;
+
+	let ideaText = ideaSlogans[ideaNum];
+	let i = 0;
+
 	let x = setInterval(() => {
-		if () 
-		clearInterval(x);
-	}, TYPE_SPEED)
+		if (i === (ideaText.length - 1)) { // Finished
+			clearInterval(x);
+			setTimeout(callback, TEXT_KEEP);
+		} else {
+			//
+		}
+		i++
+	}, TYPE_SPEED);
 }
 
 function generateNewIdeaNum() {

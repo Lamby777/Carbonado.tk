4bd6dd17492203de421c6e4a236bdc2c5ecaffe6
Dexie <35111165+Lamby777@users.noreply.github.com>
2021-11-12 07:18:26 +0000

    detypewrite structure change + less cringy slogan lines

--- page/typewrite.js
+++ page/typewrite.js
@@ -1,11 +1,14 @@
 // Used in index page to typewrite text
-const TYPE_SPEED = 100; // measured in ms per key
+const TYPE_SPEED = 40; // measured in ms per key
 const TEXT_KEEP = 5000; // time after finished to keep text
 const span = document.getElementById("homepage-edited-text");
-const ideaSlogans = [ // stereotypical corporate shit, lol
-	"A cryptocurrency for everyone",
-	"A platform for your new token",
-	"An NFT-first blockchain implementation",
+const ideaSlogans = [ // actually decent lines now
+	"A crypto for everyone!",
+	"A platform for your new token.",
+	"Digital ownership!",
+	"... or something, I guess.",
+	"An NFT-first blockchain implementation.",
+	"Official Carbonado project website!"
 ];
 
 let lastIdeaNum = -1; // Not equal to any index of ^^^
@@ -24,11 +27,24 @@ function typewrite(callback) {
 		if (i === ideaText.length) { // Finished
 			clearInterval(typeInterval);
 			if (callback) setTimeout(callback, TEXT_KEEP);
+			else setTimeout(detypewrite, TEXT_KEEP);
 		} else {
 			i++;
 			span.innerHTML = ideaText.slice(0, i);
 		}
 	}, TYPE_SPEED);
+
+	function detypewrite() {
+		let detypeInterval = setInterval(() => {
+			if (i === 0) { // Finished
+				clearInterval(detypeInterval);
+				typewrite();
+			} else {
+				i--;
+				span.innerHTML = ideaText.slice(0, i);
+			}
+		}, TYPE_SPEED);
+	}
 }
 
 typewrite();

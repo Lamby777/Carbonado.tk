dac489b6f88fcf62099f197ce7bb8ca2f3522842
Dexie <35111165+Lamby777@users.noreply.github.com>
2021-11-12 06:38:31 +0000

    typewrite RNG and setInterval

--- page/typewrite.js
+++ page/typewrite.js
@@ -1,2 +1,21 @@
 // Used in index page to typewrite text
-const typetext = document.body.getElementById("")
+const TYPE_SPEED = 100; // measured in ms per key
+const span = document.body.getElementById("homepage-edited-text");
+const ideaSlogans = [ // stereotypical corporate shit, lol
+	"A cryptocurrency for everyone",
+	"A platform for your new token",
+	"An NFT-first blockchain implementation",
+];
+
+let lastIdeaNum;
+
+function typewrite(text) {
+	let x = setInterval(() => {
+		if () 
+		clearInterval(x);
+	}, TYPE_SPEED)
+}
+
+function generateNewIdeaNum() {
+	return Math.floor(Math.random() * ideaSlogans.length);
+}
